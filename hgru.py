7d593be65fa09b142ee2fc8f85bd6326806fde69
Jiaxin Li <jiaxin.1995@icloud.com>
2020-01-04 10:49:58 -0800

    revert

--- hgru.py
+++ hgru.py
@@ -74,8 +74,7 @@ class hConvGRUCell(nn.Module):
     Generate a convolutional GRU cell
     """
 
-    def __init__(self, input_size, hidden_size, kernel_size, batchnorm=True, 
-                 timesteps=8, channel_sym=True):
+    def __init__(self, input_size, hidden_size, kernel_size, batchnorm=True, timesteps=8):
         super().__init__()
         self.padding = kernel_size // 2
         self.input_size = input_size
@@ -103,11 +102,13 @@ class hConvGRUCell(nn.Module):
 
         init.orthogonal_(self.w_gate_inh)
         init.orthogonal_(self.w_gate_exc)
-        if channel_sym:
-            self.w_gate_inh.data = (self.w_gate_inh + torch.transpose(self.w_gate_inh,1,0))*0.5
-            self.w_gate_exc.data = (self.w_gate_exc + torch.transpose(self.w_gate_exc,1,0))*0.5
-            self.w_gate_inh.register_hook(lambda grad: (grad + torch.transpose(grad,1,0))*0.5)
-            self.w_gate_exc.register_hook(lambda grad: (grad + torch.transpose(grad,1,0))*0.5)
+        
+#        self.w_gate_inh = nn.Parameter(self.w_gate_inh.reshape(hidden_size , hidden_size , kernel_size, kernel_size))
+#        self.w_gate_exc = nn.Parameter(self.w_gate_exc.reshape(hidden_size , hidden_size , kernel_size, kernel_size))
+        self.w_gate_inh.register_hook(lambda grad: (grad + torch.transpose(grad,1,0))*0.5)
+        self.w_gate_exc.register_hook(lambda grad: (grad + torch.transpose(grad,1,0))*0.5)
+#        self.w_gate_inh.register_hook(lambda grad: print("inh"))
+#        self.w_gate_exc.register_hook(lambda grad: print("exc"))
         
         init.orthogonal_(self.u1_gate.weight)
         init.orthogonal_(self.u2_gate.weight)
